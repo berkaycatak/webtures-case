ffd4864712058054ee6158cf0dcfd23c316b5faf
Berkay Çatak <berkaypng@gmail.com>
2022-03-28 15:20:19 +0300

    Auth transactions - vue.js is installed.

---
+++ database/migrations/2022_03_28_112540_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 255);
+            $table->text('description');
+            $table->dateTime('date');
+            $table->char('website_url', 255);
+            $table->enum("status", ["completed", "continues"])->default("continues");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
